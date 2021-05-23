a456aa22860fd264d5eb1356fe80c731515ed302
Douglas1688 <douglas.vasquezp@hotmail.com>
2021-05-22 23:26:42 -0500

    [WIP] Clase 8 de JS For in - For of

--- clase_8/script.js
+++ clase_8/script.js
@@ -1,9 +1,13 @@
 let nombres = ['Pedro','Juan','María','Jessenia','Douglas'];
 
-for (let elem in nombres) {
+for (let elem in nombres) { // Recorre los índices del array
     console.log(elem);
 }
 
-for (const i of nombres) {
-    console.log(i);
+for (const i of nombres) { // Recorre los valores del array
+    if (i == 'Jessenia') {
+        continue;
+    }
+    console.log(`Encontré a ${i}`);
+
 }
