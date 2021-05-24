2a7fffedeab97079f151248bda76e17de746827b
Douglas1688 <douglas.vasquezp@hotmail.com>
2021-05-23 23:23:15 -0500

    [ADD] Clase 10 de JS - Funciones - Arrow Functions

---
+++ clase_10/script.js
@@ -0,0 +1,16 @@
+// function saludar(){
+//     console.log("Hola mundo");
+// }
+
+// const saludar = () => console.log("Hola");
+
+// const saludarUsuario = (user) => console.log(`Hola ${user}`); //Arrow functiona
+// saludar();
+
+// saludarUsuario("Douglas");
+
+const suma = (n1,n2) => {
+    console.log(n1+n2);
+}
+
+suma(2,3);
