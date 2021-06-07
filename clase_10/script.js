2c8672dc5ff1b81662a0e6682fb45b2b3533f5f5
Douglas1688 <hnodouglas@hotmail.com>
2021-06-07 09:18:18 -0500

    [ADD] Clase 10 Funciones

---
+++ clase_10/script.js
@@ -0,0 +1,14 @@
+function saludar (){
+
+    console.log("Hola");
+};
+
+saludar();
+
+const saludo = () => console.log("Hola Mundo");
+
+saludo();
+
+
+const suma = (n1, n2) => n1 + n2;
+console.log(suma(1,300));
---
+++ clase_14/script.js
@@ -0,0 +1,10 @@
+///SPREAD
+const numbers = [-12,2,3,23,43,2,3]
+
+const addNumbers = (a, b, c) => {
+    console.log(a+b+c);
+}
+
+let numbersToAdd = [1,2,3];
+
+addNumbers(...numbersToAdd);
