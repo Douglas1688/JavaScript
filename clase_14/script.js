e4a0349727e3ad6d68fe6ddcd196eee0f4cf3d7d
Douglas1688 <hnodouglas@hotmail.com>
2021-06-07 10:17:53 -0500

    [ADD] Clase 14 Spread

--- clase_14/script.js
+++ clase_14/script.js
@@ -1,4 +1,4 @@
-///SPREAD
+///SPREAD  => EXPANDIR
 const numbers = [-12,2,3,23,43,2,3]
 
 const addNumbers = (a, b, c) => {
@@ -8,3 +8,44 @@ const addNumbers = (a, b, c) => {
 let numbersToAdd = [1,2,3];
 
 addNumbers(...numbersToAdd);
+
+
+// AÑADIRD UN ARRAY A OTRO ARRAY
+let users = ['javier','david','rosa','juan','mercedes'];
+let newUsers = ['marta','jaime','laura'];
+
+users.push(...newUsers);
+console.log(users);
+
+//COPIAR ARRAYS
+
+// let arr1 = [1,2,3,4];
+// let arr2 = [...arr1];
+
+// console.log(arr2);
+
+// CONCATENAR ARRAYS
+let arr1 = [1,2,3,4];
+let arr2 = [6,7,8];
+// let arrConcat = arr1.concat(arr2);
+// console.log(arrConcat);
+
+let arrConcat = [...arr1, ...arr2];
+console.log(arrConcat);
+
+//ENVIAR UN NÚMERO INDEFINIDO DE ARGUMENTOS
+const restParms = (...numbers) => {
+    console.log(numbers)
+}
+
+restParms(1,2,3,4,5,6,7,8,9);
+
+// LIBRERÍAS MATH
+
+const num = [4,6,3,6,7,8];
+console.log(Math.max(...num));
+console.log(Math.min(...num));
+
+
+// ELIMINAR ELEMENTOS DUPLICADOS
+console.log(new Set(num));
