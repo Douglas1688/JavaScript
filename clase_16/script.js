8aec96792f2ed876e7e4eafbf2c5ff70936995b3
Douglas1688 <douglas.vasquezp@hotmail.com>
2021-06-15 22:52:09 -0500

    [WIP] Clase 16 DOM.

--- clase_16/script.js
+++ clase_16/script.js
@@ -1,2 +1,25 @@
+
+//document.getElementById('id') Acceder a un elemento
+// a través de su id
+
 const title = document.getElementById('title');
-console.log(title);
+// console.log(title);
+
+// title.textContent = 'Este es el nuevo texto';
+
+
+
+/* document | element .querySelector 
+Accede al primer elemento que conincida con selector CSS */
+// const paragraph = document.querySelector('.paragraph')
+// const span = paragraph.querySelector('span')
+// console.log(span.textContent);
+
+
+// const span = document.getElementById('title').querySelector('span');
+
+// console.log(span.textContent);
+
+
+const paragraphArray = Array.from(document.querySelectorAll('.paragraph'));
+paragraphArray.map(p=>p.style.color='blue');
