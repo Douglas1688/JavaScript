db2c85047062028c66c7ba59e827ca34767887c6
Douglas1688 <douglas.vasquezp@hotmail.com>
2021-06-18 22:47:12 -0500

    [WIP] Clase 17 Atributos y clases.

--- clase_17/script.js
+++ clase_17/script.js
@@ -7,7 +7,18 @@ Clases
 element.classList.add('class,class,....)
 element.classList.remove('class','class',...)
 element.classList.toggle('class',[,force])
-
+element.classList.contains('class')
+element.classList.replace('oldClass',newClass)
 
 
 */
+const title = document.getElementById('title');
+const name = document.getElementById('name');
+
+// name.setAttribute('type','radio');
+// name.setAttribute('type','number');
+// name.setAttribute('type','date');
+
+// title.classList.add('main-title');
+title.classList.remove('main-title');
+name.value = 'Yo siempre quise ser bichote'
